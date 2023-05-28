9631425b09f6cab05ff6a63322598961fe8977b0
harendersingh <harendersingh@192.168.1.37>
2023-05-28 13:51:42 +0530

    GIVECENTRAL1

---
+++ GIVECENTRAL1/src/main/java/GIVECENTRAL1/GIVECENTRAL1/App.java
@@ -0,0 +1,13 @@
+package GIVECENTRAL1.GIVECENTRAL1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
